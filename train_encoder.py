a327118d540b3a5658db988971de7f4211cc2e5d
Aitor Arronte Alvarez <arronte@hawaii.edu>
2024-10-04 12:02:48 -1000

    fp16

--- train_encoder.py
+++ train_encoder.py
@@ -77,7 +77,7 @@ def emotion_classification_pretrained(model_name, dataset, output_dir, batch_siz
             evaluation_strategy="steps",
             num_train_epochs=int(num_epochs),
             gradient_checkpointing=True,
-            fp16=False,
+            fp16=True,
             save_steps=500,
             eval_steps=500,
             logging_steps=100,
